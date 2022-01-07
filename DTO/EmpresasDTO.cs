0d73def0ce2b55138a3e842b4b600ab78e62d727
Pedro Santos <pedrohtth65@gmail.com>
2022-01-06 21:29:01 -0300

    06/01/2022

---
+++ DTO/EmpresasDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioBackend.DTO
+{
+    public class EmpresasDTO
+    {
+        public int IdEmpresa { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+        public string Cnpj { get; set; }
+    }
+}
